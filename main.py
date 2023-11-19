fe4d418fbb0d6d9b301bb378ec3f0c86ccd66878
boyarkin.k <boyarkin.k@local.pulsar>
2023-11-19 22:32:44 +0300

    [add] Добавил все 5 сенсоров

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ if __name__ == '__main__':
 
         for ROWS in plotting:
             X.append(dt.fromisoformat(ROWS[0]))
-            Y.append([float(ROWS[1]),float(ROWS[3])])
+            Y.append([float(ROWS[1]),float(ROWS[3]),float(ROWS[5]),float(ROWS[7]),float(ROWS[9])])
 
     plt.plot(X, Y)
     plt.title('Line Graph using CSV')
     plt.xlabel('Время')
     plt.ylabel('Температура')
-    plt.legend(['sensor 1', 'sensor 2'])
+    plt.legend(['sensor 1', 'sensor 2','sensor 3','sensor 4','sensor 5'])
     plt.show()
 
 '''
